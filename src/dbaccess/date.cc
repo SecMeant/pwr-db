6e44b709c7c716b974d849ce5b244793bbc7a86d
windspring <sagan.pawel1000@gmail.com>
2020-01-05 11:55:07 +0100

    fxing functions responsible for converting time

--- src/dbaccess/date.cc
+++ src/dbaccess/date.cc
@@ -3,23 +3,23 @@
 #include <sstream>
 #include <fmt/format.h>
 #include <iomanip>
-
+#include <iostream>
 using std::chrono::duration;
 namespace app::dbaccess {
   date_t str2epoch(const char* date, const char* format)
   {
     std::stringstream ss(date);
-    std::tm dt;
-    ss >> std::get_time(&dt, format);
-    std::time_t tt = std::mktime(&dt);
-    return date_t(duration_t(tt*60));
+    std::tm ptm {};
+    ss >> std::get_time(&ptm, format);
+    ptm.tm_year+=100;
+    int64_t tt = std::mktime(&ptm)*1000;
+    return date_t(Ms(tt));
   }
 
   std::string epoch2str(const date_t& date)
   {
-    auto rawtime =  std::chrono::system_clock::to_time_t(date)/60;
-    struct tm * ptm;
-    ptm = gmtime ( &rawtime );
-    return fmt::format("{}.{}.{}", ptm->tm_year, ptm->tm_mon,ptm->tm_mday);
+    std::time_t t_c = std::chrono::system_clock::to_time_t(date);
+    auto ptm = std::localtime(&t_c);
+    return fmt::format("{:0>2}.{:0>2}.{:0>2}", ptm->tm_mday, ptm->tm_mon + 1,ptm->tm_year%100);
   }
 }
--- tests/src/db_test.cc
+++ tests/src/db_test.cc
@@ -1,7 +1,9 @@
-#include <gtest/gtest.h>
 #include "hldb.h"
 #include "dbaccess/db_connection.h"
 #include "dbaccess/db_defaults.h"
+#include "dbaccess/date.h"
+
+#include <gtest/gtest.h>
 #include <fstream>
 #include <string>
 #include <fmt/format.h>
@@ -10,6 +12,7 @@ using namespace std;
 using namespace app::logic;
 using namespace app::dbaccess;
 using namespace app::dbaccess::defaults;
+
 
 constexpr const char* DB_SCRIPT_INIT_PATH = "../dbinit/test_init.sql";
 constexpr const char* DB_DATABASE_TEST = "biuro_podrozy_test";
@@ -33,13 +36,13 @@ protected:
      {
        FAIL() << "FAIL AT READING FILE\n";
      }
-     std::string line;
-     while (std::getline(fs, line, ';'))
+     std::string query;
+     while (std::getline(fs, query, ';'))
      {
-      if (line ==""){
+      if (query ==""){
         continue;
       }
-      hldb_inst.m_dbconn.query_res(line.c_str());
+      hldb_inst.m_dbconn.query_res(query.c_str());
      }
    }
    hldb hldb_inst;
@@ -60,6 +63,19 @@ TEST(DB_TEST, basicDatabaseConnection)
     DB_HOSTNAME, DB_DATABASE, DB_PORT_NO, { DB_USERNAME_LOPRIO, DB_PASSWORD_LOPRIO });
   EXPECT_EQ(true, c)<< "LO_PRIO ACCOUNT INACCESSIBLE";
 }
+
+TEST(DB_UTILS, dateConverterTest)
+{
+  string d_str = "05.01.20";
+  uint64_t time_mill = 1578178800000;
+
+  date_t base_date = date_t(Ms(time_mill));
+  EXPECT_EQ(d_str, epoch2str(base_date));
+
+  uint64_t time_conv = str2epoch(d_str.c_str(), "%d.%m.%y").time_since_epoch().count();
+  EXPECT_EQ(time_mill, time_conv);
+}
+
 
 TEST_F(HldbTest, CostomerManagerTest){
   auto customers = hldb_inst.get_all_customers();
@@ -126,3 +142,70 @@ TEST_F(HldbTest, CostomerManagerTest){
   customer = hldb_inst.get_customers_like(customer.id);
   EXPECT_EQ(customer.pesel, "000000000000")<< "INCORRECT CUSTOMERS PESEL";
 }
+
+
+// TEST_F(HldbTest, EmployeeManagerTest){
+//   auto customers = hldb_inst.get_all_customers();
+//   EXPECT_EQ(customers.size(), 0)<< "INCORRECT CUSTOMERS COUNT";
+//   employee_t  employee;
+//   employee.name ="Mario_1";
+//   employee.surname ="Bros_1";
+//   employee.email ="Mario_1.bros@op.pl";
+//   employee.pesel ="111111111111";
+//   hldb_inst.add_employee(employee);
+//   employee.name ="Mario_2";
+//   employee.surname ="Bros_2";
+//   employee.email ="Mario_2.bros@op.pl";
+//   employee.pesel ="222222222222";
+//   hldb_inst.add_employee(employee);
+//   employee.name ="Mario_3";
+//   employee.surname ="Bros_3";
+//   employee.email ="Mario_3.bros@op.pl";
+//   employee.pesel ="333333333333";
+//   hldb_inst.add_employee(employee);
+//   employee.name ="Mario_4";
+//   employee.surname ="Bros_4";
+//   employee.email ="Mario_4.bros@op.pl";
+//   employee.pesel ="444444444444";
+//   hldb_inst.add_employee(employee);
+//   employees = hldb_inst.get_all_employees();
+//   EXPECT_EQ(employees.size(), 4)<< "INCORRECT employeeS COUNT";
+
+//   employee.name ="Mario_4";
+//   employee.surname ="Bros_4";
+//   employee.email ="Mario_4.bros@op.pl";
+//   employee.pesel ="444444444444";
+//   hldb_inst.add_employee(employee);
+//   employees = hldb_inst.get_all_employees();
+//   EXPECT_EQ(employees.size(), 5)<< "INCORRECT employeeS COUNT";
+//   employee.name ="Mario_4";
+//   employee.surname ="Bros_4";
+//   employee.email ="Mario_4.bros@op.pl";
+//   employee.pesel ="444444444444";
+//   hldb_inst.add_employee(employee);
+
+//   employees = hldb_inst.get_employees_like(employee);
+//   EXPECT_EQ(employees.size(), 3)<< "INCORRECT employeeS COUNT";
+//   employee = employees[0];
+
+//   employee = hldb_inst.get_employees_like(employee.id);
+//   EXPECT_NE(employee.id, 0)<< "INCORRECT employeeS ID";
+
+//   hldb_inst.rmeove_employee(employee);
+//   employees = hldb_inst.get_employees_like(employee);
+//   EXPECT_EQ(employees.size(), 2)<< "INCORRECT employeeS COUNT";
+
+//   employee.id = 0;
+//   hldb_inst.rmeove_employee(employee);
+//   employees = hldb_inst.get_employees_like(employee);
+//   EXPECT_EQ(employees.size(), 0)<< "INCORRECT employeeS COUNT";
+
+//   employee.id = 1;
+//   employee.name ="Mario_M";
+//   employee.surname ="Bros_M";
+//   employee.email ="Mario_M.bros@op.pl";
+//   employee.pesel ="000000000000";
+//   hldb_inst.modify_employee(employee);
+//   employee = hldb_inst.get_employees_like(employee.id);
+//   EXPECT_EQ(employee.pesel, "000000000000")<< "INCORRECT CUSTOMERS PESEL";
+// }
