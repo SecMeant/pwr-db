117dcfb812ea3bf7dbadab8662474c0c867dc7f3
windspring <sagan.pawel1000@gmail.com>
2020-01-05 21:04:05 +0100

    ui impl

---
+++ gui/bdg/windows.cmake
@@ -0,0 +1,9 @@
+set(QT_INSTALL_DIR "C:/Qt")
+set(COMILER_DIRECTORY "${QT_INSTALL_DIR}/Tools/mingw730_64/bin")
+# variable necessary to locate qt cmake config file for find packge
+set(CMAKE_PREFIX_PATH "${QT_INSTALL_DIR}/5.12.2/mingw73_64/bin")
+set(Qt5Core_DIR "${QT_INSTALL_DIR}/5.12.2/mingw73_64/lib/cmake/Qt5Core")
+
+set(CMAKE_C_COMPILER "${COMILER_DIRECTORY}/gcc.exe")
+set(CMAKE_CXX_COMPILER "${COMILER_DIRECTORY}/g++.exe")
+set(CMAKE_AR "${COMILER_DIRECTORY}/gcc-ar.exe")
