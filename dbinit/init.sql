b293b782554cffcdc2e6b277ce37ab0fa37a1347
secmeant <secmeant@gmail.com>
2020-01-06 12:39:33 +0100

    Updating sql init script

--- dbinit/init.sql
+++ dbinit/init.sql
@@ -1,5 +1,27 @@
-CREATE USER 'superuser'@'localhost' IDENTIFIED BY 'alamakota';
-GRANT ALL PRIVILEGES ON *.* TO 'superuser'@'localhost';
+DROP USER 'root'@'localhost';
+CREATE USER 'root'@'localhost' IDENTIFIED BY 'alamakota';
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost';
+FLUSH PRIVILEGES;
+
+CREATE USER 'loprio'@'localhost' IDENTIFIED BY 'bazydanych19';
+GRANT USAGE ON *.* TO 'loprio'@'localhost';
+REVOKE SELECT, ALTER, CREATE, DELETE, DROP, INSERT, INDEX, REFERENCES, UPDATE  ON TABLE *.* FROM 'loprio'@'localhost';
+GRANT SELECT, SHOW VIEW  ON TABLE `biuro_podrozy`.`credentials` TO 'loprio'@'localhost';
+FLUSH PRIVILEGES;
+
+CREATE USER 'miprio'@'localhost' IDENTIFIED BY 'bazydanych19';
+GRANT USAGE ON *.* TO 'miprio'@'localhost';
+REVOKE SELECT, ALTER, CREATE, DELETE, DROP, INSERT, INDEX, REFERENCES, UPDATE  ON TABLE *.* FROM 'miprio'@'localhost';
+GRANT ALL PRIVILEGES  ON TABLE `biuro_podrozy`.`annual_income` TO 'miprio'@'localhost';
+GRANT ALL PRIVILEGES  ON TABLE  `biuro_podrozy`.`category` TO 'miprio'@'localhost';
+GRANT ALL PRIVILEGES  ON TABLE  `biuro_podrozy`.`customers` TO 'miprio'@'localhost';
+GRANT ALL PRIVILEGES  ON TABLE  `biuro_podrozy`.`offers` TO 'miprio'@'localhost';
+GRANT ALL PRIVILEGES  ON TABLE  `biuro_podrozy`.`tour` TO 'miprio'@'localhost';
+FLUSH PRIVILEGES;
+
+CREATE USER 'hiprio'@'localhost' IDENTIFIED BY 'bazydanych19';
+GRANT USAGE ON *.* TO 'hiprio'@'localhost';
+GRANT ALL PRIVILEGES  ON TABLE `biuro_podrozy`.* TO 'hiprio'@'localhost';
 FLUSH PRIVILEGES;
 
 drop database IF EXISTS biuro_podrozy_test;
