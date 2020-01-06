2e758b24471fbe017c215531218f7b694aa919ee
secmeant <secmeant@gmail.com>
2020-01-06 19:58:29 +0100

    users now have privileges to test database

--- dbinit/users.sql
+++ dbinit/users.sql
@@ -2,6 +2,7 @@ create user 'loprio'@'localhost' identified by 'bazydanych19';
 grant usage on *.* to 'loprio'@'localhost';
 revoke select, alter, create, delete, drop, insert, index, references, update  on table *.* from 'loprio'@'localhost';
 grant select, show view  on table `biuro_podrozy`.`credentials` to 'loprio'@'localhost';
+grant select, show view  on table `biuro_podrozy_test`.`credentials` to 'loprio'@'localhost';
 flush privileges;
 
 create user 'miprio'@'localhost' identified by 'bazydanych19';
@@ -12,9 +13,15 @@ grant all privileges  on table  `biuro_podrozy`.`category` to 'miprio'@'localhos
 grant all privileges  on table  `biuro_podrozy`.`customers` to 'miprio'@'localhost';
 grant all privileges  on table  `biuro_podrozy`.`offers` to 'miprio'@'localhost';
 grant all privileges  on table  `biuro_podrozy`.`tour` to 'miprio'@'localhost';
+grant all privileges  on table `biuro_podrozy_test`.`annual_income` to 'miprio'@'localhost';
+grant all privileges  on table  `biuro_podrozy_test`.`category` to 'miprio'@'localhost';
+grant all privileges  on table  `biuro_podrozy_test`.`customers` to 'miprio'@'localhost';
+grant all privileges  on table  `biuro_podrozy_test`.`offers` to 'miprio'@'localhost';
+grant all privileges  on table  `biuro_podrozy_test`.`tour` to 'miprio'@'localhost';
 flush privileges;
 
 create user 'hiprio'@'localhost' identified by 'bazydanych19';
 grant usage on *.* to 'hiprio'@'localhost';
 grant all privileges  on table `biuro_podrozy`.* to 'hiprio'@'localhost';
+grant all privileges  on table `biuro_podrozy_test`.* to 'hiprio'@'localhost';
 flush privileges;
