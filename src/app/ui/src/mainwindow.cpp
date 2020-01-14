f11086ce972be2fef22ebb65b108a52f96bdec9f
secmeant <secmeant@gmail.com>
2020-01-14 16:18:55 +0100

    Adding implementation of offert tab

--- src/app/ui/src/mainwindow.cpp
+++ src/app/ui/src/mainwindow.cpp
@@ -116,17 +116,96 @@ void MainWindow::on_customer_results_cellClicked(int row, int column)
 
 void MainWindow::on_offer_id_textChanged(const QString &arg1)
 {
+  if (arg1.isEmpty())
+    this->ui->offer_add->setText("Add");
+  else
+    this->ui->offer_add->setText("Modify");
+}
 
+static offer_t
+offer_parse_info(Ui::MainWindow *ui)
+{
+  auto new_offer = offer_t::make_any();
+
+  if (!ui->offer_id->text().isEmpty())
+    new_offer.id = ui->offer_name->text().toInt();
+
+  if (!ui->offer_name->text().isEmpty())
+    new_offer.name = ui->offer_name->text().toInt();
+
+  if (!ui->offer_country->text().isEmpty())
+    new_offer.country = ui->offer_country->text().toInt();
+
+  if (!ui->offer_city->text().isEmpty())
+    new_offer.city = ui->offer_city->text().toInt();
+
+  if (!ui->offer_price->text().isEmpty())
+    new_offer.price = ui->offer_price->text().toInt();
+
+  if (!ui->offer_from->text().isEmpty())
+    new_offer.date_begin = str2epoch(ui->offer_from->text().toStdString().c_str());
+
+  if (!ui->offer_to->text().isEmpty())
+    new_offer.date_end = str2epoch(ui->offer_to->text().toStdString().c_str());
+
+  if (!ui->offer_insurance_cost->text().isEmpty())
+    new_offer.insurance_cost = ui->offer_insurance_cost->text().toInt();
+
+  if (!ui->offer_extra_meals_cost->text().isEmpty())
+    new_offer.extra_meals_cost = ui->offer_extra_meals_cost->text().toInt();
+
+  if (!ui->offer_ticket_count->text().isEmpty())
+    new_offer.tickets_count = ui->offer_ticket_count->text().toInt();
+
+  return new_offer;
 }
 
 void MainWindow::on_offer_add_released()
 {
+  auto new_offer = offer_parse_info(this->ui);
 
+  if (sql::any(new_offer.id)) { // Add
+    this->logic.add_offer(new_offer);
+  } else { // Modify
+    this->logic.modify_offer(new_offer);
+  }
+
+}
+
+static void
+offer_result_add_row(QTableWidget *table, const offer_t &offer, hldb_i &logic)
+{
+  std::array<QTableWidgetItem *, offer_t::field_count> row;
+
+  row[0] = new QTableWidgetItem(QString(offer.id));
+  row[1] = new QTableWidgetItem(offer.name.c_str());
+  row[2] = new QTableWidgetItem(offer.country.c_str());
+  row[3] = new QTableWidgetItem(offer.city.c_str());
+  row[4] = new QTableWidgetItem(offer.price);
+  row[5] = new QTableWidgetItem(epoch2str(offer.date_begin).c_str());
+  row[6] = new QTableWidgetItem(epoch2str(offer.date_end).c_str());
+  row[7] = new QTableWidgetItem(offer.insurance_cost);
+  row[8] = new QTableWidgetItem(offer.extra_meals_cost);
+  row[9] = new QTableWidgetItem(offer.tickets_count);
+  row[10] = new QTableWidgetItem(logic.get_category_by_id(offer.categoryid).name.c_str());
+
+  auto row_index = table->rowCount();
+  table->insertRow(row_index);
+
+  for (auto i = 0ull; i < row.size(); ++i)
+    table->setItem(row_index, i, row[i]);
 }
 
 void MainWindow::on_offer_search_released()
 {
+  auto new_offer = offer_parse_info(this->ui);
 
+  auto offers = this->logic.get_offers_like(new_offer);
+
+  this->ui->offer_results->clear();
+
+  for (auto &c : offers)
+    offer_result_add_row(this->ui->offer_results, c, this->logic);
 }
 
 void MainWindow::on_offer_results_cellClicked(int row, int column)
