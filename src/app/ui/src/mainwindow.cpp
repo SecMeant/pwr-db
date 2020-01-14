5a162a35cb5a95a5440170e07a56790d012303cb
windspring <sagan.pawel1000@gmail.com>
2020-01-14 12:14:24 +0100

    updating ui, adding signals

--- src/app/ui/src/mainwindow.cpp
+++ src/app/ui/src/mainwindow.cpp
@@ -12,3 +12,140 @@ MainWindow::~MainWindow()
 {
   delete ui;
 }
+
+void MainWindow::on_customer_id_textChanged(const QString &arg1)
+{
+
+}
+
+void MainWindow::on_customer_add_released()
+{
+
+}
+
+void MainWindow::on_customer_search_released()
+{
+
+}
+
+void MainWindow::on_customer_results_cellClicked(int row, int column)
+{
+
+}
+
+void MainWindow::on_offer_id_textChanged(const QString &arg1)
+{
+
+}
+
+void MainWindow::on_offer_add_released()
+{
+
+}
+
+void MainWindow::on_offer_search_released()
+{
+
+}
+
+void MainWindow::on_offer_results_cellClicked(int row, int column)
+{
+
+}
+
+void MainWindow::on_tour_customer_id_textChanged(const QString &arg1)
+{
+
+}
+
+void MainWindow::on_tour_customer_search_released()
+{
+
+}
+
+void MainWindow::on_tour_offer_id_textChanged(const QString &arg1)
+{
+
+}
+
+void MainWindow::on_tour_offer_search_released()
+{
+
+}
+
+void MainWindow::on_tour_customer_result_cellClicked(int row, int column)
+{
+
+}
+
+void MainWindow::on_tour_offer_result_cellClicked(int row, int column)
+{
+
+}
+
+void MainWindow::on_tour_with_insurance_toggled(bool checked)
+{
+
+}
+
+void MainWindow::on_tour_with_extra_melas_toggled(bool checked)
+{
+
+}
+
+void MainWindow::on_buy_tour_released()
+{
+
+}
+
+void MainWindow::on_tour_id_textChanged(const QString &arg1)
+{
+
+}
+
+
+void MainWindow::on_tour_insurance_toggled(bool checked)
+{
+
+}
+
+void MainWindow::on_tour_extra_meals_toggled(bool checked)
+{
+
+}
+
+void MainWindow::on_tour_search_released()
+{
+
+}
+
+void MainWindow::on_tour_resign_released()
+{
+
+}
+
+void MainWindow::on_tour_results_cellClicked(int row, int column)
+{
+
+}
+
+void MainWindow::on_employee_id_textChanged(const QString &arg1)
+{
+
+}
+
+void MainWindow::on_employee_add_released()
+{
+
+}
+
+void MainWindow::on_employee_search_released()
+{
+
+}
+
+void MainWindow::on_employee_results_cellClicked(int row, int column)
+{
+
+}
+
--- src/app/ui/src/mainwindow.h
+++ src/app/ui/src/mainwindow.h
@@ -15,6 +15,61 @@ public:
   explicit MainWindow(QWidget *parent = nullptr);
   ~MainWindow();
 
+private slots:
+    void on_customer_id_textChanged(const QString &arg1);
+
+    void on_customer_add_released();
+
+    void on_customer_search_released();
+
+    void on_customer_results_cellClicked(int row, int column);
+
+    void on_offer_id_textChanged(const QString &arg1);
+
+    void on_offer_add_released();
+
+    void on_offer_search_released();
+
+    void on_offer_results_cellClicked(int row, int column);
+
+    void on_tour_customer_id_textChanged(const QString &arg1);
+
+    void on_tour_customer_search_released();
+
+    void on_tour_offer_id_textChanged(const QString &arg1);
+
+    void on_tour_offer_search_released();
+
+    void on_tour_customer_result_cellClicked(int row, int column);
+
+    void on_tour_offer_result_cellClicked(int row, int column);
+
+    void on_tour_with_insurance_toggled(bool checked);
+
+    void on_tour_with_extra_melas_toggled(bool checked);
+
+    void on_buy_tour_released();
+
+    void on_tour_insurance_toggled(bool checked);
+
+    void on_tour_id_textChanged(const QString &arg1);
+
+    void on_tour_extra_meals_toggled(bool checked);
+
+    void on_tour_search_released();
+
+    void on_tour_resign_released();
+
+    void on_tour_results_cellClicked(int row, int column);
+
+    void on_employee_id_textChanged(const QString &arg1);
+
+    void on_employee_add_released();
+
+    void on_employee_search_released();
+
+    void on_employee_results_cellClicked(int row, int column);
+
 private:
   Ui::MainWindow *ui;
 };
